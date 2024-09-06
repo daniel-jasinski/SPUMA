d84e7b1fefdaf18ff6c6c10821b7e890166d6811
Giuseppe Giaquinto <g.giaquinto@cineca.it>
2024-09-06 16:27:09 +0200

    small fix: - mod numb of block dispatched in cuda backend for - remove variadic argument from forAllOwner[neighbour] macros

--- src/OpenFOAM/device/executor/cudaExecutor.cu
+++ src/OpenFOAM/device/executor/cudaExecutor.cu
@@ -99,7 +99,7 @@ void Foam::cudaExecutor::_backendFor(F& lambda, const label& size)
     const label numblocks = SET_NUM_BLOCKS(size);
 
     Foam::cuda::lambdaKernel<F>
-    <<<numblocks,NUM_THREADS_PER_BLOCK>>>
+    <<<(numblocks + NUM_SM -1)/ NUM_SM,NUM_THREADS_PER_BLOCK>>>
     (lambda,size);
 
     deviceSync(); 
