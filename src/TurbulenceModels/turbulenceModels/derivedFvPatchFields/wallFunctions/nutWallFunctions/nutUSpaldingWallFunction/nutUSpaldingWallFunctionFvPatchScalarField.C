d04ad3810f969d320535f1c10caa9279efb63538
Giuseppe Giaquinto <g.giaquinto@cineca.it>
2024-10-04 11:37:13 +0200

    feature: add executor to nuUSpaldingWallFunction

--- src/TurbulenceModels/turbulenceModels/derivedFvPatchFields/wallFunctions/nutWallFunctions/nutUSpaldingWallFunction/nutUSpaldingWallFunctionFvPatchScalarField.C
+++ src/TurbulenceModels/turbulenceModels/derivedFvPatchFields/wallFunctions/nutWallFunctions/nutUSpaldingWallFunction/nutUSpaldingWallFunctionFvPatchScalarField.C
@@ -79,13 +79,24 @@ Foam::nutUSpaldingWallFunctionFvPatchScalarField::calcNut() const
         // tolerance.
 
         scalarField& nutw = tnutw.ref();
-        forAll(err, facei)
-        {
-            if (err[facei] < tolerance_)
+        // TODO executor
+        foamExecutor exec;
+        auto nutwp = nutw.begin();
+        const auto errp = err.cbegin();
+        auto Lambda = [=](label facei){
+            if (errp[facei] < tolerance_)
             {
-                nutw[facei] = this->operator[](facei);
-            }
-        }
+                nutwp[facei] = this->operator[](facei);
+            } 
+        };
+        exec.parallelFor(Lambda,err.size());
+        // forAll(err, facei)
+        // {
+        //     if (err[facei] < tolerance_)
+        //     {
+        //         nutw[facei] = this->operator[](facei);
+        //     }
+        // }
     }
     return tnutw;
 }
@@ -140,57 +151,120 @@ Foam::nutUSpaldingWallFunctionFvPatchScalarField::calcUTau
     err.setSize(uTau.size());
     err = 0.0;
 
-    forAll(uTau, facei)
+    //TODO executor
+    foamExecutor exec;
+    auto errp = err.begin();
+    auto uTaup = uTau.begin();
+    
+    auto magUpp = magUp.begin();
+    auto magGradUp = magGradU.begin();
+    auto nuwp = nuw.begin();
+    auto nutwp = nutw.begin();
+    auto yp = y.begin();
+
+    auto Lambda = [=](label facei)
     {
-        scalar ut = sqrt((nutw[facei] + nuw[facei])*magGradU[facei]);
+        scalar ut = sqrt((nutwp[facei] + nuwp[facei])*magGradUp[facei]);
         // Note: for exact restart seed with laminar viscosity only:
         //scalar ut = sqrt(nuw[facei]*magGradU[facei]);
-
+        
+        // const auto a = errp[facei];
+        // const auto b = uTaup[facei];
+        // const auto c = magUpp[facei];
+        // const auto d = magGradUp[facei];
+        // const auto e = nuwp[facei];
+        // const auto f = nutwp[facei];
+        // const auto g = yp[facei];
+        
         if (ROOTVSMALL < ut)
         {
             int iter = 0;
 
             do
             {
-                const scalar kUu = min(kappa*magUp[facei]/ut, scalar(50));
+                const scalar kUu = min(kappa*magUpp[facei]/ut, scalar(50));
                 const scalar fkUu = exp(kUu) - 1 - kUu*(1 + 0.5*kUu);
 
                 const scalar f =
-                    - ut*y[facei]/nuw[facei]
-                    + magUp[facei]/ut
+                    - ut*yp[facei]/nuwp[facei]
+                    + magUpp[facei]/ut
                     + 1.0/E*(fkUu - 1.0/6.0*kUu*sqr(kUu));
 
                 const scalar df =
-                    y[facei]/nuw[facei]
-                  + magUp[facei]/sqr(ut)
+                    yp[facei]/nuwp[facei]
+                  + magUpp[facei]/sqr(ut)
                   + 1.0/E*kUu*fkUu/ut;
 
                 const scalar uTauNew = ut + f/df;
-                err[facei] = mag((ut - uTauNew)/ut);
+                errp[facei] = Foam::mag((ut - uTauNew)/ut);
                 ut = uTauNew;
-
-                //iterations_++;
 
             } while
             (
                 ut > ROOTVSMALL
-             && err[facei] > tolerance_
+             && errp[facei] > tolerance_
              && ++iter < maxIter
             );
 
-            uTau[facei] = max(scalar(0), ut);
-
-            //invocations_++;
-            //if (iter > 1)
-            //{
-            //    nontrivial_++;
-            //}
-            //if (iter >= maxIter_)
-            //{
-            //    nonconvergence_++;
-            //}
+            uTaup[facei] = max(scalar(0), ut);
         }
-    }
+
+    };
+
+    // exec.parallelFor(Lambda,uTau.size());
+
+
+    // forAll(uTau, facei)
+    // {
+    //     scalar ut = sqrt((nutw[facei] + nuw[facei])*magGradU[facei]);
+    //     // Note: for exact restart seed with laminar viscosity only:
+    //     //scalar ut = sqrt(nuw[facei]*magGradU[facei]);
+
+    //     if (ROOTVSMALL < ut)
+    //     {
+    //         int iter = 0;
+
+    //         do
+    //         {
+    //             const scalar kUu = min(kappa*magUp[facei]/ut, scalar(50));
+    //             const scalar fkUu = exp(kUu) - 1 - kUu*(1 + 0.5*kUu);
+
+    //             const scalar f =
+    //                 - ut*y[facei]/nuw[facei]
+    //                 + magUp[facei]/ut
+    //                 + 1.0/E*(fkUu - 1.0/6.0*kUu*sqr(kUu));
+
+    //             const scalar df =
+    //                 y[facei]/nuw[facei]
+    //               + magUp[facei]/sqr(ut)
+    //               + 1.0/E*kUu*fkUu/ut;
+
+    //             const scalar uTauNew = ut + f/df;
+    //             err[facei] = mag((ut - uTauNew)/ut);
+    //             ut = uTauNew;
+
+    //             //iterations_++;
+
+    //         } while
+    //         (
+    //             ut > ROOTVSMALL
+    //          && err[facei] > tolerance_
+    //          && ++iter < maxIter
+    //         );
+
+    //         uTau[facei] = max(scalar(0), ut);
+
+    //         //invocations_++;
+    //         //if (iter > 1)
+    //         //{
+    //         //    nontrivial_++;
+    //         //}
+    //         //if (iter >= maxIter_)
+    //         //{
+    //         //    nonconvergence_++;
+    //         //}
+    //     }
+    // }
 
     return tuTau;
 }
