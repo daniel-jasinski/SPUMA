c577f3ce79ca7752e6b5c964962c5e55f3bce7dd
Simone Bna <sbna0000@login01.leonardo.local>
2025-01-30 12:51:31 +0100

    Added Richardson smoother

---
+++ src/OpenFOAM/matrices/lduMatrix/smoothers/Richardson/RichardsonSmoother.C
@@ -0,0 +1,166 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2011-2015 OpenFOAM Foundation
+    Copyright (C) 2017-2019 OpenCFD Ltd.
+    Copyright (C) 2025 CINECA
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "RichardsonSmoother.H"
+#include "PrecisionAdaptor.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    defineTypeNameAndDebug(RichardsonSmoother, 0);
+
+    lduMatrix::smoother::addsymMatrixConstructorToTable<RichardsonSmoother>
+        addRichardsonSmootherSymMatrixConstructorToTable_;
+
+    lduMatrix::smoother::addasymMatrixConstructorToTable<RichardsonSmoother>
+        addRichardsonSmootherAsymMatrixConstructorToTable_;
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::RichardsonSmoother::RichardsonSmoother
+(
+    const word& fieldName,
+    const lduMatrix& matrix,
+    const FieldField<Field, scalar>& interfaceBouCoeffs,
+    const FieldField<Field, scalar>& interfaceIntCoeffs,
+    const lduInterfaceFieldPtrsList& interfaces
+)
+:
+    lduMatrix::smoother
+    (
+        fieldName,
+        matrix,
+        interfaceBouCoeffs,
+        interfaceIntCoeffs,
+        interfaces
+    )
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void Foam::RichardsonSmoother::smooth
+(
+    const word& fieldName_,
+    solveScalarField& psi,
+    const lduMatrix& matrix_,
+    const solveScalarField& source,
+    const FieldField<Field, scalar>& interfaceBouCoeffs_,
+    const lduInterfaceFieldPtrsList& interfaces_,
+    const direction cmpt,
+    const label nSweeps
+)
+{
+    solveScalar* __restrict__ psiPtr = psi.begin();
+    const solveScalar* const __restrict__ bPtr = source.cbegin();
+
+    const label nCells = psi.size();
+
+    const scalar* const __restrict__ diagPtr = matrix_.diag().cbegin();
+
+    solveScalarField Apsi(nCells);
+    solveScalar* __restrict__ ApsiPtr = Apsi.begin();
+
+    scalarField rD(nCells);
+    scalar* __restrict__ rDPtr = rD.begin();
+
+    foamExecutor exec;
+
+    // -- Calculate the inverse of the diagonal matrix (D^-1)
+    auto Lambda1 = [=](label celli)
+    {
+        rDPtr[celli] = 1. / diagPtr[celli];
+    };
+    exec.parallelFor(Lambda1, nCells);
+
+    const scalar omega = 0.75; 
+
+    for (label sweep=0; sweep<nSweeps; sweep++)
+    {
+        // --- Calculate A.psi
+        matrix_.Amul(Apsi, psi, interfaceBouCoeffs_, interfaces_, cmpt);
+
+	// --- Update solution (iteration of the damped jacobi method)
+        auto Lambda2 = [=](label celli)
+        {
+	    psiPtr[celli] += omega * rDPtr[celli] * (bPtr[celli] - ApsiPtr[celli]);
+        };
+        exec.parallelFor(Lambda2, nCells);
+    }
+}
+
+
+void Foam::RichardsonSmoother::smooth
+(
+    solveScalarField& psi,
+    const scalarField& source,
+    const direction cmpt,
+    const label nSweeps
+) const
+{
+    smooth
+    (
+        fieldName_,
+        psi,
+        matrix_,
+        ConstPrecisionAdaptor<solveScalar, scalar>(source),
+        interfaceBouCoeffs_,
+        interfaces_,
+        cmpt,
+        nSweeps
+    );
+}
+
+
+void Foam::RichardsonSmoother::scalarSmooth
+(
+    solveScalarField& psi,
+    const solveScalarField& source,
+    const direction cmpt,
+    const label nSweeps
+) const
+{
+    smooth
+    (
+        fieldName_,
+        psi,
+        matrix_,
+        source,
+        interfaceBouCoeffs_,
+        interfaces_,
+        cmpt,
+        nSweeps
+    );
+}
+
+
+// ************************************************************************* //
